cb4b8b2a4f0a885670e7efa18bda232d0433a901
Kevin Qiu <kzqiu01@gmail.com>
2023-03-24 00:08:52 -0400

    Added new strategy for bananas. (does not work well. ;(

--- round2/tester.py
+++ round2/tester.py
@@ -4,6 +4,8 @@ import json
 from datamodel import Order, OrderDepth, ProsperityEncoder, TradingState, Symbol 
 from typing import Any, Dict, List
 
+#################################################################################
+
 class Logger:
     def __init__(self) -> None:
         self.logs = ""
@@ -28,205 +30,16 @@ class Logger:
 
 logger = Logger()
 
-"""
-ARIMA Functions!
-"""
-def least_squares(x, y):
-    if np.linalg.det(x.T @ x) != 0:
-        return np.linalg.inv((x.T @ x)) @ (x.T @ y)
-    return np.linalg.pinv((x.T @ x)) @ (x.T @ y) 
+#################################################################################
 
-# Autoregressor
-def ar_process(eps, phi):
-    """
-    Creates a AR process with a zero mean.
-    """
-    # Reverse the order of phi and add a 1 for current eps_t
-    phi = np.r_[1, phi][::-1]
-    ar = eps.copy()
-    offset = len(phi)
-    for i in range(offset, ar.shape[0]):
-        ar[i - 1] = ar[i - offset: i] @ phi
-    return ar
+# Mid-price estimators
+def mid_price(best_bid: int, best_ask: int) -> float:
+    return (best_bid + best_ask) / 2
 
-# Moving Average 
-n = 500
-eps = np.random.normal(size=n)
+def weighted_mid_price(best_bid: int, qty_bid: int, best_ask: int, qty_ask: int) -> float:
+    imbalance = qty_bid / (qty_bid + qty_ask)
 
-def lag_view(x, order):
-    """
-    For every value X_i create a row that lags k values: [X_i-1, X_i-2, ... X_i-k]
-    """
-    y = x.copy()
-    # Create features by shifting the window of `order` size by one step.
-    # This results in a 2D array [[t1, t2, t3], [t2, t3, t4], ... [t_k-2, t_k-1, t_k]]
-    x = np.array([y[-(i + order):][:order] for i in range(y.shape[0])])
-
-    # Reverse the array as we started at the end and remove duplicates.
-    # Note that we truncate the features [order -1:] and the labels [order]
-    # This is the shifting of the features with one time step compared to the labels
-    x = np.stack(x)[::-1][order - 1: -1]
-    y = y[order:]
-
-    return x, y
-
-def ma_process(eps, theta):
-    """
-    Creates an MA(q) process with a zero mean (mean not included in implementation).
-    :param eps: (array) White noise signal.
-    :param theta: (array/ list) Parameters of the process.
-    """
-    # reverse the order of theta as Xt, Xt-1, Xt-k in an array is Xt-k, Xt-1, Xt.
-    theta = np.array([1] + list(theta))[::-1][:, None]
-    eps_q, _ = lag_view(eps, len(theta))
-    return eps_q @ theta
-
-# Differencing 
-def difference(x, d=1):
-    if d == 0:
-        return x
-    else:
-        x = np.r_[x[0], np.diff(x)]
-        return difference(x, d - 1)
-
-def undo_difference(x, d=1):
-    if d == 1:
-        return np.cumsum(x)
-    else:
-        x = np.cumsum(x)
-        return undo_difference(x, d - 1)
-
-# Linear Regression
-class LinearModel:
-    def __init__(self, fit_intercept=True):
-        self.fit_intercept = fit_intercept
-        self.beta = None
-        self.intercept_ = None
-        self.coef_ = None
-
-    def _prepare_features(self, x):
-        if self.fit_intercept:
-            x = np.hstack((np.ones((x.shape[0], 1)), x))
-        return x
-
-    def fit(self, x, y):
-        x = self._prepare_features(x)
-        self.beta = least_squares(x, y)
-        if self.fit_intercept:
-            self.intercept_ = self.beta[0]
-            self.coef_ = self.beta[1:]
-        else:
-            self.coef_ = self.beta
-
-    def predict(self, x):
-        x = self._prepare_features(x)
-        return x @ self.beta
-
-    def fit_predict(self, x, y):
-        self.fit(x, y)
-        return self.predict(x)
-
-# ARIMA Model 
-class ARIMA(LinearModel):
-    def __init__(self, q, d, p):
-        """
-        An ARIMA model.
-        :param q: (int) Order of the MA model.
-        :param p: (int) Order of the AR model.
-        :param d: (int) Number of times the data needs to be differenced.
-        """
-        super().__init__(True)
-        self.p = p
-        self.d = d
-        self.q = q
-        self.ar = None
-        self.resid = None
-
-    def prepare_features(self, x):
-        if self.d > 0:
-            x = difference(x, self.d)
-
-        ar_features = None
-        ma_features = None
-
-        # Determine the features and the epsilon terms for the MA process
-        if self.q > 0:
-            if self.ar is None:
-                self.ar = ARIMA(0, 0, self.p)
-                self.ar.fit_predict(x)
-            eps = self.ar.resid
-            eps[0] = 0
-
-            # prepend with zeros as there are no residuals_t-k in the first X_t
-            ma_features, _ = lag_view(np.r_[np.zeros(self.q), eps], self.q)
-
-        # Determine the features for the AR process
-        if self.p > 0:
-            # prepend with zeros as there are no X_t-k in the first X_t
-            ar_features = lag_view(np.r_[np.zeros(self.p), x], self.p)[0]
-
-        if ar_features is not None and ma_features is not None:
-            n = min(len(ar_features), len(ma_features))
-            ar_features = ar_features[:n]
-            ma_features = ma_features[:n]
-            features = np.hstack((ar_features, ma_features))
-        elif ma_features is not None:
-            n = len(ma_features)
-            features = ma_features[:n]
-        else:
-            n = len(ar_features)
-            features = ar_features[:n]
-
-        return features, x[:n]
-
-    def fit(self, x):
-        features, x = self.prepare_features(x)
-        super().fit(features, x)
-        return features
-
-    def fit_predict(self, x):
-        """
-        Fit and transform input
-        :param x: (array) with time series.
-        """
-        features = self.fit(x)
-        return self.predict(x, prepared=(features))
-
-    def predict(self, x, **kwargs):
-        """
-        :param x: (array)
-        :kwargs:
-            prepared: (tpl) containing the features, eps and x
-        """
-        features = kwargs.get('prepared', None)
-        if features is None:
-            features, x = self.prepare_features(x)
-
-        y = super().predict(features)
-        self.resid = x - y
-
-        return self.return_output(y)
-
-    def return_output(self, x):
-        if self.d > 0:
-            x = undo_difference(x, self.d)
-        return x
-
-    def forecast(self, x, n):
-        """
-        Forecast the time series.
-        :param x: (array) Current time steps.
-        :param n: (int) Number of time steps in the future.
-        """
-        features, x = self.prepare_features(x)
-        y = super().predict(features)
-
-        # Append n time steps as zeros. Because the epsilon terms are unknown
-        y = np.r_[y, np.zeros(n)]
-        for i in range(n):
-            feat = np.r_[y[-(self.p + n) + i: -n + i], np.zeros(self.q)]
-            y[x.shape[0] + i] = super().predict(feat[None, :])
-        return self.return_output(y)
+    return imbalance * best_ask + (1 - imbalance) * best_bid
 
 # Edge Spread Estimator!
 def edge(open: np.array, high: np.array, low: np.array, close: np.array) -> float:
@@ -280,22 +93,26 @@ def edge(open: np.array, high: np.array, low: np.array, close: np.array) -> floa
     s2 = -4 * (w1 * e1 + w2 * e2) / ((1 - k * (n1 + n2) / 2) + (1 - n5) * (1 - k * (n3 + n4) / 2))
     return float(max(0, s2) ** 0.5)
 
-##################################################################################
+# slope momentum calculation
+def slope_momentum(period: int, mid_prices: np.array) -> float:
+    period = min(period, len(mid_prices))
+    diffs = np.array(mid_prices[-period + 1:]) - np.array(mid_prices[-period:-1]) # prev
 
-# historical data!
+    return np.mean(diffs)
+    # return sum([diffs[i] * np.exp(i + 1) for i in range(-1, -period + 1, -1)])
+
+##################################################################################
 
 class Trader:
     def __init__(self):
-        self.holdings = 0
         self.last_trade = 0
 
-        self.history_length = 20
-        self.price_history_banana = np.array([])
-        self.open_prices = np.array([])
-        self.high_prices = np.array([])
-        self.low_prices = np.array([]) 
-        self.close_prices = np.array([])
-        self.model = ARIMA(4, 0, 1)
+        self.prices = []
+        self.open = []
+        self.high = []
+        self.low = []
+        self.close = []
+        self.rsi = []
 
     def run(self, state: TradingState) -> dict[Symbol, List[Order]]:
         result = {}
@@ -304,96 +121,99 @@ class Trader:
             orders: list[Order] = []
                 
             if product == 'BANANAS':
-                # starting at certain timestamp
-                enough_data = True
-                start_trading = 0
-
+                # current position
                 position_limit = 20
                 current_position = state.position.get(product, 0)
 
-                order_depth: OrderDepth = state.order_depths[product]
+                buy_orders = state.order_depths[product].buy_orders
+                sell_orders = state.order_depths[product].sell_orders
                 orders: list[Order] = []
-                    
-                price = 0
-                count = 0
-                for Trade in state.market_trades.get(product, []):
-                    price += Trade.price * Trade.quantity
-                    count += Trade.quantity
 
-                if count == 0:
-                    if len(self.price_history_banana) == 0:
-                        enough_data = False
-                    else:
-                        current_avg_market_price = self.price_history_banana[-1]
+                best_bid = max(buy_orders.keys())
+                best_ask = min(sell_orders.keys())
+
+                # Getting fair price, uses last trade by default
+                fair_price = self.last_trade
+
+                # If there are valid buy and sell orders, then calculate mean of mid and weighted mid prices
+                if len(buy_orders) > 0 and len(sell_orders) > 0:
+                    fair_price = (mid_price(best_bid, best_ask) + weighted_mid_price(best_bid, buy_orders[best_bid], best_ask, -sell_orders[best_ask])) / 2
+                
+                # If fair price is still default value, just skip this iteration!
+                if fair_price == 0:
+                    continue
+
+                # Getting spread!
+                # intervals to look back on for open/high/low/close
+                n = 20
+
+                self.open.append(fair_price)
+                self.close.append(fair_price)
+
+                if state.timestamp == 0:
+                    self.high.append(fair_price)
+                    self.low.append(fair_price)
                 else:
-                    current_avg_market_price = price / count
-           
+                    self.high.append(max(self.high[-1], fair_price))
+                    self.low.append(max(self.low[-1], fair_price))
+                
+                if len(self.open) > n:
+                    self.high = self.high[1:]
+                    self.low = self.low[1:]
+                    self.open = self.open[1:]
+                    self.close = self.close[1:]
 
-                if state.timestamp >= start_trading and enough_data == True:
-                    # Calculating fair price
-                    self.price_history_banana = np.append(self.price_history_banana, current_avg_market_price)
-                    pred = self.model.fit_predict(self.price_history_banana)
+                # Trading code
+                if state.timestamp > 1000:
+                
+                    p_spread = edge(self.open, self.high, self.low, self.close)
 
-                    if len(pred) >= self.history_length + 1:
-                        pred = pred[1:]
+                    momentum = slope_momentum(3, self.open)
 
-                    forecasted_price = self.model.forecast(pred, 1)[-1]
+                    # FULL BID-ASK SPREAD RANGE!
+                    spread = p_spread * fair_price + momentum
 
-                    # Calculating spread
-                    spread = 4
+                    # parameter for volume to purchase
+                    volume_alpha = 5
 
-                    buy_spread = spread / 2
-                    sell_spread = spread / 2
+                    sells = sorted([it for it in sell_orders.items()], reverse=True)
+                    buys = sorted([it for it in buy_orders.items()], reverse=True)
 
-                    sell_orders = sorted([sell_ord for sell_ord in order_depth.sell_orders.items()], reverse=True)
-                    buy_orders = sorted([buy_ord for buy_ord in order_depth.buy_orders.items()], reverse=True)
+                    agg_vol = current_position
 
-                    sell_pos = current_position
-                    buy_pos = current_position
-                    breakloop = False
-
-                    for order in sell_orders:
-                        if sell_pos == 20:
+                    for order in sells:
+                        if agg_vol == 20:
                             break
 
                         ask = order[0]
                         vol = order[1]
 
-                        if sell_pos - vol > position_limit:
-                            vol = sell_pos - position_limit
-                            breakloop = True
-                        
-                        if ask <= forecasted_price - buy_spread and -vol > 0:
-                            print("BUY", product, str(-vol) + "x", ask)
-                            orders.append(Order(product, ask, -vol))
-                            sell_pos += -vol
+                        # TODO: Add position component!
+                        desired_vol = round(momentum * volume_alpha * spread / (ask - fair_price))
+                        real_vol = min(desired_vol, position_limit - agg_vol, -vol)
 
-                        if breakloop:
-                            break
+                        if ask <= fair_price + spread / 2:
+                            print("BUY", product, str(real_vol) + "x", ask)
+                            agg_vol += real_vol 
+                            orders.append(Order(product, ask, real_vol))
 
-                    breakloop = False
+                    agg_vol = current_position
 
-                    for order in buy_orders:
-                        if current_position == -20:
+                    for order in buys:
+                        if agg_vol == -20:
                             break
 
                         bid = order[0]
                         vol = order[1]
 
-                        if buy_pos - vol < -position_limit:
-                            vol = buy_pos + position_limit
-                            breakloop = True
+                        # TODO: Add position component!
+                        desired_vol = round(momentum * volume_alpha * spread / (fair_price - bid))
+                        real_vol = max(desired_vol, -position_limit - agg_vol, -vol)
 
-                        if bid >= forecasted_price + sell_spread and vol > 0:
-                            print("SELL", product, str(vol) + "x", bid)
-                            orders.append(Order(product, bid, -vol))
-                            buy_pos -= vol
-
-                        if breakloop:
-                            break
-
-                    # add open contracts here! 
-                    # check if sell_pos exceeds 20 and buy_pos exceeds -20
+                        if bid >= fair_price - spread / 2:
+                            print("SELL", product, str(real_vol) + "x", bid)
+                            agg_vol += real_vol
+                            orders.append(Order(product, bid, real_vol))
 
             result[product] = orders
         logger.flush(state, orders)
