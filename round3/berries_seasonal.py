b352d3aeb19436d17579f7e0a2f29c80d9ce8a84
Kevin Qiu <kzqiu01@gmail.com>
2023-03-24 22:51:18 -0400

    Added proper bounds for berries strat.

--- round3/berries_seasonal.py
+++ round3/berries_seasonal.py
@@ -51,11 +51,11 @@ class Trader:
                 - Hold at midday and switch positions!
                 """
 
-                if time >= 100000 and time < 110000 and position != position_limit:
+                if time >= 90000 and time < 120000 and position != position_limit:
                     best_ask = min(state.order_depths[product].sell_orders.items())
                     orders.append(Order(product, best_ask[0], min(-best_ask[1], position_limit - position)))
                 
-                if time >= 500000 and time < 510000 and position != -position_limit:
+                if time >= 500000 and time < 505000 and position != -position_limit:
                     best_bid = max(state.order_depths[product].buy_orders.items())
                     orders.append(Order(product, best_bid[0], max(-best_bid[1], - position_limit - position)))
 
